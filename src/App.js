63280ba9c0d84838441a11914d4acd1a137cc23f
enricvelasco <enric.velasco.g@gmail.com>
2019-11-15 13:59:15 +0100

    add logo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { GlobalStyle } from './GlobalStyles';
 import { ListOfCategories } from './components/list-of-categories';
 import { ListOfPhotoCards } from './components/list-of-photocards';
+import { Logo } from './components/logo';
 
 const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
