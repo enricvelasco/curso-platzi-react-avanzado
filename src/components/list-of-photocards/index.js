fc319add153b3c02bd7ea21ef13fe7260a795d20
enric.velasco.g@gmail.com <Supertetas24>
2019-11-18 22:51:54 +0100

    add graphql

--- src/components/list-of-photocards/index.js
+++ src/components/list-of-photocards/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import { PhotoCard } from '../photo-card';
+import { graphql } from 'react-apollo';
+import { gql } from 'apollo-boost'; //permite hacer las queries como si fuera un string
 
-export const ListOfPhotoCards = () => (
+const withPhotos = graphql(gql(`
+query getPhotos {
+  photos {
+    id
+    categoryId
+    src
+    likes
+    userId
+    liked
+  }
+}
+`))
+
+const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => (
   <ul>
     {
-      [1, 2, 3, 4, 5, 6, 7].map(id => <PhotoCard key={id} id={id} />)
+      photos.map(photo => <PhotoCard key={photo.id} {...photo} />)
     }
   </ul>
 );
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
