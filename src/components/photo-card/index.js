b480424ace4281ebc9f128c848552f887f001f71
enric.velasco.g@gmail.com <Supertetas24>
2019-11-20 23:07:40 +0100

    add likes with mutation

--- src/components/photo-card/index.js
+++ src/components/photo-card/index.js
@@ -1,17 +1,16 @@
 import React, { Fragment } from 'react';
-import { MdFavorite, MdFavoriteBorder } from 'react-icons/md';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { useNearScreen } from '../../hooks/useNearSceen';
-import { Article, Button, ImageWrapper, Img } from './styles';
+import { Article, ImageWrapper, Img } from './styles';
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/toggle-like-mutation'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE}) => {
   const [show, element] = useNearScreen();
-  const key = 'like-' + id;
+  const key = 'like-' + id
   const [liked, setLiked] = useLocalStorage(key, false);
-  const Icon = liked ? MdFavorite : MdFavoriteBorder;
-
   //a Article se le poone una altura minima pq si no lo pone a 0 de salida y lo detecta como que lo está mostrando en el viewport
   return (
     <Article ref={element}>
@@ -23,9 +22,21 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE}) => {
                 <Img src={src} />
               </ImageWrapper>
             </a>
-            <Button onClick={() => setLiked(!liked)}>
-              <Icon size='32px' />{likes} likes!
-            </Button>
+            <ToggleLikeMutation>
+              {
+                (toggleLike) => {
+                  const handleFavClick = () => {
+                    !liked && toggleLike({
+                      variables: {
+                        input: { id }
+                      }
+                    })
+                    setLiked(!liked)
+                  }
+                  return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+                }
+              }
+            </ToggleLikeMutation>
           </Fragment>
       }
     </Article>
