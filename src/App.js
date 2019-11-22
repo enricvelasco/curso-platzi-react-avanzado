3c4444190cd7a0f5c3171f707ba762be52b26799
enricvelasco <enric.velasco.g@gmail.com>
2019-11-22 14:52:53 +0100

    implement protected path

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from 'react'
 import { Router } from '@reach/router'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/logo'
+
+import { NavBar } from './components/nav-bar'
+
 import { Home } from './pages/home'
 import { Detail } from './pages/detail'
-import { NavBar } from './components/nav-bar'
+import { Favs } from './pages/favs'
+import { User } from './pages/user'
+import { NotRegisteredUser } from './pages/not-registered-user'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
 
 const App = () => {
   return (
@@ -16,6 +25,21 @@ const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth ?
+              <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              :
+              <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+        }
+      </UserLogged>
       <NavBar />
     </div>
   );
