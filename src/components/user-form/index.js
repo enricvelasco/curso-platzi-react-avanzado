130b634aa1f3c9ccea2c73c5a234eba9337112ad
enric.velasco.g@gmail.com <Supertetas24>
2019-11-23 16:58:23 +0100

    add styles in form

--- src/components/user-form/index.js
+++ src/components/user-form/index.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { useInputValue } from '../../hooks/useInputValue'
+import { Form, Input, Button, Title } from './style'
 
-export const UserForm = ({ onSubmit }) => {
+export const UserForm = ({ onSubmit, title }) => {
   const email = useInputValue('')
   const password = useInputValue('')
   return (
-    <form onSubmit={onSubmit}>
-      {/* eslint-disable-next-line react/jsx-handler-names */}
-      <input placeholder='Email' value={email.value} onChange={email.onChange} />
-      {/* eslint-disable-next-line react/jsx-handler-names */}
-      <input placeholder='Password' type='password' value={password.value} onChange={password.onChange} />
-      <button>Iniciar Sesión</button>
-    </form>
+    <>
+      <Title>{title}</Title>
+      <Form onSubmit={onSubmit}>
+        {/* eslint-disable-next-line react/jsx-handler-names */}
+        <Input placeholder='Email' value={email.value} onChange={email.onChange} />
+        {/* eslint-disable-next-line react/jsx-handler-names */}
+        <Input placeholder='Password' type='password' value={password.value} onChange={password.onChange} />
+        <Button>{title}</Button>
+      </Form>
+    </>
   )
 }
