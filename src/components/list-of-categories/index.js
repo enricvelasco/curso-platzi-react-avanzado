fc9d59866113994bca6fc5d3108524945d18418e
enricvelasco <enric.velasco.g@gmail.com>
2019-11-29 13:52:40 +0100

    add react Memo

--- src/components/list-of-categories/index.js
+++ src/components/list-of-categories/index.js
@@ -18,7 +18,7 @@ const useCategoriesData = () => {
   return { categories, loading };
 };
 
-export const ListOfCategories = () => {
+const ListOfCategoriesComponent = () => {
   const { categories, loading } = useCategoriesData();
   const [showFixed, setShowFixed] = useState(false);
 
@@ -50,3 +50,5 @@ export const ListOfCategories = () => {
     </Fragment>
   );
 }
+
+export const ListOfCategories = React.memo(ListOfCategoriesComponent)
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,9 +3,13 @@ import { ListOfCategories } from '../components/list-of-categories'
 import { ListOfPhotoCards } from '../container/list-of-photocards'
 import { Layout } from '../components/layout'
 
-export const Home = ({ categoryId }) => (
+const HomePage = ({ categoryId }) => (
   <Layout title='Tu app de fotos de mascotas' subtitle='Con Petgram puedes encontrar fotos de animales domesticos muy bonitos'>
     <ListOfCategories />
     <ListOfPhotoCards categoryId={categoryId} />
   </Layout>
 )
+
+export const Home = React.memo(HomePage, (prevProps, props) => {
+  return prevProps.categoryId === props.categoryId
+})
